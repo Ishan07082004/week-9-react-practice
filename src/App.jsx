c48b2e6a842ef23462667a3be6189dca6bebc12d
Ishan Verma <ishanv002@gmail.com>
2025-01-15 20:29:23 +0530

    understanding keys in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
-// import { useState, useEffect } from "react"
+import { useState } from "react"
 
-function App() {
-  // const [showTimer, setShowTimer] = useState(true);
+// const App = () => {
+//   const todos = [{
+//     title: "Go to gym",
+//     done: true
+//   }, {
+//     title: "Eat food",
+//     done: false
+//   }];
 
-  return <div style={{display: "flex"}}>
-    <Card>
-      {<div style={{color: "green"}}>
-        What's up <br/> <br/>
-        <input type={"text"} />
-      </div>}
-    </Card>
-    <Card>
-      Heyyy!!!
-    </Card>
+//   const todosComponent = todos.map(todo => <Todo title={todo.title} done={todo.done} />)
+
+//   return (
+//     <div>
+//       {todosComponent}
+//     </div>
+//   );
+// };
+
+const App = () => {
+  return (
+    <div>
+      {[
+        <Todo key={1} title={"Go to gym"} done = {false} />,
+        <Todo key={2} title={"Eat Healthy"} done = {true} />
+      ]}
+    </div>
+  );
+};
+
+function Todo({title, done}) {
+  return <div>
+    {title} - {done ? "Done" : "Not done"}
   </div>
 }
 
-function Card( {children} ) {
-  return <div style={{background: "black", borderRadius: 10, color: "white", padding: 10, margin: 10}}>
-    {children}
-  </div>
-}
+// function App() {
+//   // const [showTimer, setShowTimer] = useState(true);
+
+//   return <div style={{display: "flex"}}>
+//     <Card>
+//       {<div style={{color: "green"}}>
+//         What's up <br/> <br/>
+//         <input type={"text"} />
+//       </div>}
+//     </Card>
+//     <Card>
+//       Heyyy!!!
+//     </Card>
+//   </div>
+// }
+
+// function Card( {children} ) {
+//   return <div style={{background: "black", borderRadius: 10, color: "white", padding: 10, margin: 10}}>
+//     {children}
+//   </div>
+// }
 
 export default App
